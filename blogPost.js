1a141b247e2bc6c23686a5a235dc58b746c2a2e6
narendrasoni1989 <narendrasoni2@gmail.com>
2014-09-21 16:32:34 +0800

    first commit

---
+++ blogPost.js
@@ -0,0 +1,13 @@
+module.exports = function(db){
+	var mongoose = require('mongoose');
+	mongoose.connect("mongodb://" + db.host + "/" + db.database);
+	var schema = new mongoose.Schema({
+		title : String,
+		body : String,
+		date : {
+			type : Date,
+			default : Date.now
+		}
+	});
+	return mongoose.model("BlogPost",schema);
+};
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+ // res.send('response from index page');
+ // res.render('index.ejs');
+  res.render('index.ejs');
+};
