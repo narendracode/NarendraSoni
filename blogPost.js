d9d5c0b064fda73e616283c069a25fb31ae6b4cf
narendrasoni1989 <narendrasoni2@gmail.com>
2014-09-21 17:39:14 +0800

    db host change

--- blogPost.js
+++ blogPost.js
@@ -1,6 +1,7 @@
 module.exports = function(db){
 	var mongoose = require('mongoose');
-	mongoose.connect("mongodb://" + db.host + "/" + db.database);
+	//mongoose.connect("mongodb://" + db.host + "/" + db.database);
+	mongoose.connect("mongodb://narendra:2013Singapore@kahana.mongohq.com:10012/"+db.database);
 	var schema = new mongoose.Schema({
 		title : String,
 		body : String,
